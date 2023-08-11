01d95e79ce8a57b458b16bca53cd292fd29c7811
Lucas Murakami <lucaskaitomurakami@gmail.com>
2023-08-11 15:54:34 -0300

    Compra Simples, com Carrinho Vazio e Múltipla

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -12,8 +12,54 @@ class CaixaDaLanchonete {
         "combo2": {description: 'Café', price: 7.50},
     }
 
-    calcularValorDaCompra(metodoDePagamento, itens) {              
-        return '';
+    //TAXA OU DESCONTO
+    calcularTaxaOuDesconto(metodoDePagamento, itensPrice) {
+        switch(metodoDePagamento){
+            case 'dinheiro':
+
+                return (itensPrice - (itensPrice * 0.05)).toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});     
+
+            case 'debito':
+
+                return itensPrice.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'}); 
+
+            case 'credito':
+
+                return (itensPrice + (itensPrice * 0.03)).toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});                
+            
+            default:
+                return 'Forma de pagamento inválida!';
+        }
+    }
+
+    calcularTotalDeItens(itens) {
+        let itensPrice = 0;
+        
+        for (let index = 0; index < itens.length; index++) {
+            const item = itens[index];
+            const itemName = item.split(",")[0];
+            const quantity = item.split(",")[1];
+                        
+            if(itemName in this.menu){
+                itensPrice += (this.menu[itemName].price * quantity);
+            }else{
+                return 'Item inválido!';
+            }
+            
+        }
+        return itensPrice;
+    }
+
+    calcularValorDaCompra(metodoDePagamento, itens) {     
+        if(itens.length == 0){return 'Não há itens no carrinho de compra!';}
+
+        let itensPrice = this.calcularTotalDeItens(itens);   
+        //console.log(itensPrice);   
+        if(typeof itensPrice === "string") {return itensPrice}
+
+        let price = this.calcularTaxaOuDesconto(metodoDePagamento, itensPrice);
+                    
+        return price;
     }
 
 }
