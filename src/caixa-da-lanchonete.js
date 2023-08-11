4bb2c18a7c3e43c1dac036dcec34e981d692f5a1
Lucas Murakami <lucaskaitomurakami@gmail.com>
2023-08-11 14:34:48 -0300

    Desafio Iniciado + Menu

---
+++ src/caixa-da-lanchonete.js
@@ -0,0 +1,21 @@
+class CaixaDaLanchonete {
+
+    //DATA
+    menu = {
+        "cafe": {description: 'Café', price: 3.00},
+        "chantily": {description: 'Chantily (extra do café)', price: 1.50},
+        "suco": {description: 'Café', price: 6.20},
+        "sanduiche": {description: 'Café', price: 6.50},
+        "queijo": {description: 'Café', price: 2.00},
+        "salgado": {description: 'Café', price: 7.25},
+        "combo1": {description: 'Café', price: 9.25},
+        "combo2": {description: 'Café', price: 7.50},
+    }
+
+    calcularValorDaCompra(metodoDePagamento, itens) {              
+        return '';
+    }
+
+}
+
+export { CaixaDaLanchonete };
